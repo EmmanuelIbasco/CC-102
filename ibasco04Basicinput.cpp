95fb823d234be0f1ef038c6593bcb3262fea4231
Emmanuel Ibasco <emmanuelmibasco@gmail.com>
2024-10-07 09:23:10 +0800

    Create ibasco04Basicinput.cpp

---
+++ ibasco04Basicinput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string nickname, course_year_level, school;
+	cout << "nickname:" ;
+	cin >> nickname;
+	cout << "course_year levels: ";
+	cin >> course_year_level;
+	cout << "school: ";
+	cin >> school;
+	return 0;
+}
