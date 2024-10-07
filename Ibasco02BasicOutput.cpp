9de921cb316db86e985bba5328fb3eb786d76f67
Emmanuel Ibasco <emmanuelmibasco@gmail.com>
2024-10-07 09:05:49 +0800

    Create Ibasco02BasicOutput.cpp

---
+++ Ibasco02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout<< "School\t\t\t	:	Our Lady of Lourdes College Foundation." << endl;
+	cout<< "Department\t\t\t:	OLLCF College of Computer Studies" << endl;
+	cout<< "Course & Year Level\t\t:	BSIT 1-C" << endl;
+	cout<< "Full Name\t\t	:	Emmanuel Ibasco" << endl;
+	cout<< "Address\t\t\t	:	P9 Brgy 8, Daet Camarines Norte" << endl;
+	
+return 0;
+}
